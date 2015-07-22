f9ccc5307cc2fe33689afad6a6beb59aaad09947
antonioisas87 <antonioisas87@gmail.com>
2015-07-22 18:02:03 -0400

    Add ex4.c

---
+++ ex4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// broken program on pourpose 
+
+int main()
+{
+	int age =10;
+	int height=75;
+	
+	printf("I am %d years old.\n",age);
+	printf("I am %d inches tall.\n", height);
+	
+	return 0;
+}
